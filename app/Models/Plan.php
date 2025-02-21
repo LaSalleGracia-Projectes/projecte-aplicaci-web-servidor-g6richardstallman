18759360f1cec69652c7878ec6319636256fa43a
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-21 19:21:10 +0100

    add plan model

---
+++ app/Models/Plan.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Plan extends Model
+{
+    protected $table = 'plan';
+    protected $primaryKey = 'idPlan';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nombrePlan',
+        'funcionalitats'
+    ];
+
+    // Relación con Pago
+    public function pagos(): HasMany
+    {
+        return $this->hasMany(Pago::class, 'tipoPlan');
+    }
+
+    // Relación con Estadistiques
+    public function estadistiques(): HasMany
+    {
+        return $this->hasMany(Estadistiques::class, 'tipusPlan');
+    }
+}
