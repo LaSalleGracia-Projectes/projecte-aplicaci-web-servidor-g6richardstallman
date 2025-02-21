ff71b389e84809e86f114c27c03d2ab89d4d0921
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-21 19:21:50 +0100

    add evento model

---
+++ app/Models/Evento.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Evento extends Model
+{
+    protected $table = 'evento';
+    protected $primaryKey = 'idEvento';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'idEmpresa',
+        'nombreEvento',
+        'fechaEvento',
+        'lugar'
+    ];
+
+    // Relación con Empresa
+    public function empresa(): BelongsTo
+    {
+        return $this->belongsTo(Empresa::class, 'idEmpresa');
+    }
+
+    // Relación con Entrada
+    public function entradas(): HasMany
+    {
+        return $this->hasMany(Entrada::class, 'idEvento');
+    }
+}
