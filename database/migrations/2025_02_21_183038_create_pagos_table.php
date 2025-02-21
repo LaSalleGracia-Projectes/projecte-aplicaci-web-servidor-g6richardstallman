bb321d57e69c3c30d954bef26446141431ec5808
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-21 19:31:19 +0100

    add pago migration

---
+++ database/migrations/2025_02_21_183038_create_pagos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('pago', function (Blueprint $table) {
+        $table->id('idPago');
+        $table->string('nombre');
+        $table->string('contacto')->nullable();
+        $table->string('telefono', 20)->nullable();
+        $table->string('email')->unique()->nullable();
+        $table->integer('tipoPlan')->nullable();
+        $table->foreign('tipoPlan')->references('idPlan')->on('plan')->onDelete('set null');
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pagos');
+    }
+};
