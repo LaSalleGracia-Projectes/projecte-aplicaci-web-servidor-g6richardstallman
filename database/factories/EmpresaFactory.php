dcb9c6ce9221834dc024d04709bac5be8a843dda
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-21 20:48:12 +0100

    add empresa factory

---
+++ database/factories/EmpresaFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Empresa>
+ */
+class EmpresaFactory extends Factory
+{
+    protected $model = Empresa::class;
+
+public function definition()
+    {
+        return [
+            'nombre' => $this->faker->company,
+            'contacto' => $this->faker->name,
+            'telefono' => $this->faker->phoneNumber,
+            'email' => $this->faker->unique()->companyEmail,
+            'tipoPlan' => $this->faker->numberBetween(1, 3)
+        ];
+    }
+}
