d6f5c67d14ba1296c501cdf6714cf39788dad7e8
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-21 20:55:50 +0100

    add entradafactory

---
+++ database/factories/EntradaFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Entrada>
+ */
+class EntradaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Estadistiques::class;
+
+    public function definition()
+    {
+        return [
+            'idEmpresa' => Empresa::inRandomOrder()->first()->idEmpresa,
+            'idEvento' => Evento::inRandomOrder()->first()->idEvento,
+            'visitesTotals' => $this->faker->numberBetween(0, 1000),
+            'entradesVendes' => $this->faker->numberBetween(0, 1000),
+            'tipusPlan' => Plan::inRandomOrder()->first()->idPlan,
+            'valoracioProm' => $this->faker->randomFloat(2, 0, 5)
+        ];
+    }
+}
