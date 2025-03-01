a74c92d9353d99ff56a7974cf67a9a33afec08c7
yagoalonso1 <yagoalonso3105@gmail.com>
2025-03-01 20:08:42 +0100

    cascade deletes into userid

--- database/migrations/2025_02_21_182453_create_organizadores_table.php.php
+++ database/migrations/2025_02_21_182453_create_organizadores_table.php.php
@@ -4,27 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateOrganizadorTable extends Migration
 {
-    public function up(): void
+    public function up()
     {
         Schema::create('organizador', function (Blueprint $table) {
-            $table->bigIncrements('idOrganizador');
+            $table->id('idOrganizador');
             $table->string('nombre_organizacion');
             $table->string('telefono_contacto');
-            $table->unsignedBigInteger('user_id')->unique();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
             $table->softDeletes();
 
+            // Relación con cascada
             $table->foreign('user_id')
-                ->references('idUser')
-                ->on('users')
-                ->onDelete('cascade');
+                  ->references('idUser')
+                  ->on('users')
+                  ->onDelete('cascade');
         });
     }
 
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('organizador');
     }
-};
+}
